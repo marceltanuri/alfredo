d18724d6c8b57b88328f078197f7f41ab714a654
Marcel Tanuri <marcel.tanuri@gmail.com>
2022-05-27 10:07:08 +0100

    upd publish.sh commit history is disabled for static site

--- publish.sh
+++ publish.sh
@@ -2,5 +2,9 @@
 
 echo "Publishing static content to GitPages..."
 cd $1
+git checkout --orphan latest_branch
+git add -A
 git commit -a -m'update site content'
-git push
+git branch -D main
+git branch -m main
+git push -f origin main
